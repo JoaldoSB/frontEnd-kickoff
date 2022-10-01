4213c8af0995603fb0589a7e71161cedc873df74
Joaldo Santos <joaldo.einstein@hotmail.com>
2022-10-01 16:32:38 -0300

    Inserção de estilo

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -666,30 +666,45 @@ parcelHelpers.export(exports, "getDeleteButton", ()=>getDeleteButton);
 parcelHelpers.export(exports, "getCheckbox", ()=>getCheckbox);
 function getTodoItem({ id , name , status , onDelete , onUpdate  }) {
     const element = document.createElement("div");
-    element.append(getCheckbox({
+    element.className = "row mt-2";
+    const elementComplete = document.createElement("div");
+    elementComplete.className = "col-4";
+    elementComplete.append(getCheckbox({
         status,
         onClick: onUpdate
     }));
-    element.append(name);
-    element.append(getDeleteButton({
+    element.append(elementComplete);
+    const elementName = document.createElement("div");
+    elementName.className = "col-4";
+    elementName.append(name);
+    element.append(elementName);
+    const elementDelete = document.createElement("div");
+    elementDelete.className = "col-4 d-flex flex-row-reverse";
+    elementDelete.append(getDeleteButton({
         onClick: onDelete
     }));
+    element.append(elementDelete);
     return element;
 }
 function getDeleteButton({ onClick  }) {
     const button = document.createElement("button");
-    button.innerHTML = "Remover";
     button.onclick = ()=>{
         button.disabled = "disabled";
         onClick();
     };
+    button.className = "btn btn-danger ml-2";
+    const elementSpamDelete = document.createElement("spam");
+    elementSpamDelete.className = "material-icons";
+    elementSpamDelete.innerHTML = "delete";
+    button.append(elementSpamDelete);
     return button;
 }
 function getCheckbox({ status , onClick  }) {
-    const button = document.createElement("button");
-    button.innerHTML = status === "pending" ? "complete" : "undo";
+    const button = document.createElement("input");
+    button.setAttribute("type", "checkbox");
     button.onclick = ()=>{
-        onClick();
+        const check = button.checked ? true : false;
+        button.checked = check;
     };
     return button;
 }
--- src/elements.js
+++ src/elements.js
@@ -1,31 +1,53 @@
 export function getTodoItem({ id, name, status, onDelete, onUpdate }){
+
     const element = document.createElement('div');
-    
-    element.append(getCheckbox({ status, onClick: onUpdate }));
-    element.append(name);
-    element.append(getDeleteButton({ onClick: onDelete }))
+    element.className = "row mt-2";    
+    const elementComplete = document.createElement("div");
+    elementComplete.className = "col-4";
+    elementComplete.append(getCheckbox({ status, onClick: onUpdate }));
+  
+    element.append(elementComplete);
+  
+    const elementName = document.createElement("div");
+    elementName.className = "col-4";
+    elementName.append(name);
+  
+    element.append(elementName);
+  
+    const elementDelete = document.createElement("div");
+    elementDelete.className = "col-4 d-flex flex-row-reverse";
+    elementDelete.append(getDeleteButton({ onClick: onDelete }));
+  
+    element.append(elementDelete);
     return element;
-}
-
-export function getDeleteButton ({ onClick }){
-    const button = document.createElement('button');
-    button.innerHTML = 'Remover';
+  }
+  
+  export function getDeleteButton({ onClick }) {
+    const button = document.createElement("button");
   
     button.onclick = () => {
-      button.disabled = 'disabled';
-      onClick();
-    }
-    
-    return button;
-}
-
-export function getCheckbox({ status, onClick }){
-    const button = document.createElement('button');
-    button.innerHTML = status === 'pending' ? 'complete' : 'undo';
-  
-    button.onclick = () => {
+      button.disabled = "disabled";
       onClick();
     };
-    
+  
+    button.className = "btn btn-danger ml-2";
+  
+    const elementSpamDelete = document.createElement("spam");
+    elementSpamDelete.className = "material-icons";
+    elementSpamDelete.innerHTML = "delete";
+    button.append(elementSpamDelete);
+  
     return button;
+  }
+  
+  export function getCheckbox({ status, onClick }) {
+    const button = document.createElement("input");
+    button.setAttribute("type", "checkbox");
+    
+    button.onclick = () => {
+      const check = button.checked ? true : false;
+      button.checked = check;
+    };
+  
+  return button;
 }
